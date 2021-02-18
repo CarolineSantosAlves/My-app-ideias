cdb33726ed259156ed29d1b3c2e8613af67ab360
Caroline Santos <caroline.santos.wb@gmail.com>
2021-02-18 14:55:12 -0300

    refatoração

--- Projetos/Bin2Dec/app.js
+++ Projetos/Bin2Dec/app.js
@@ -1,15 +1,19 @@
-//função converter numero
-function bin2dec(){
-    let bin = document.getElementById('bin').value;
-    let res = document.getElementById('res');
-    let lista = document.querySelector('p#flista')
+const myform = document.getElementById('formNum');
+let lista = document.getElementById('result');
 
+
+
+myform.addEventListener('submit', function(e){
+    e.preventDefault();
+    
+    let bin = document.getElementById('bin').value; 
+    
     let dec = 0;
     let converteu = [];
-
+    
     function valida(){
-        let binar = Number(bin);
-        let regx = /^[0-1]{1,8}$/.test(binar);
+        
+        let regx = /^[0-1]{1,8}$/.test(bin);
         if(regx == true){
             return true;
         }else{
@@ -17,36 +21,27 @@ function bin2dec(){
         }
     }
     
-   
-   
-  
-    if(valida() == true){
-
-         for (let c = 0; c < bin.length; c++) {
-            dec += Math.pow(2, c) * bin[bin.length - c - 1]; //pega do último ao primeiro  
-                                    
-        }
-        
-        converteu.push(Number(dec));
-        let item = document.createElement('option');
-        item.text = `${bin} convertido para (${dec}) base 10`;
-        lista.appendChild(item);
-        res.innerHTML = '';
-        
-            
     
+    
+    
+    if(valida() == true){
+        for (let c = 0; c < bin.length; c++) {
+            dec += Math.pow(2, c) * bin[bin.length - c - 1]; 
+        }
+        converteu.push(dec);
+        let item = document.createElement('p');
+        item.innerText = `${bin} convertido para ${dec}`;
+        lista.appendChild(item);
+        
+         
+        
     } else{
         window.alert('valor inválido, digite até 8 numeros usando apenas 0 e 1')
     };
-
-    bin.value = '';
-    bin.focus();
+    
+    document.getElementById('bin').value = ''
+    document.getElementById('bin').focus();
      
        
-    
-    
 
-    
-    
-    
-}
+})
